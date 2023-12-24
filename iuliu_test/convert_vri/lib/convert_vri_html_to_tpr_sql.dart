dc9b7e044b0fbdb95b69de56c20a20382bd65454
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2023-12-24 18:02:06 -0500

    extract functions

--- iuliu_test/convert_vri/lib/convert_vri_html_to_tpr_sql.dart
+++ iuliu_test/convert_vri/lib/convert_vri_html_to_tpr_sql.dart
@@ -6,8 +6,8 @@ List<String> cut_vri_html_into_myanmar_edition_pages(String vriHtml) {
   var elements = document.body!.nodes.whereType<Element>();
 
   var pages = elements.fold<List<List<String>>>([[]], (pages, element) {
-    var isFirstPage = element.querySelectorAll('a').any((a) => a.attributes['name'] == "M0.0001");
-    var isNewPage = element.querySelectorAll('a').any((a) => a.attributes['name']?.startsWith('M') ?? false);
+    var isFirstPage = containsFirstPage(element);
+    var isNewPage = containsNewPage(element);
 
     if (isNewPage && !isFirstPage) {
       return [...pages, [element.outerHtml]];
@@ -18,4 +18,12 @@ List<String> cut_vri_html_into_myanmar_edition_pages(String vriHtml) {
   });
 
   return pages.map((page) => page.join('\n')).toList();
+}
+
+bool containsFirstPage(Element element) {
+  return element.querySelectorAll('a').any((a) => a.attributes['name'] == "M0.0001");
+}
+
+bool containsNewPage(Element element) {
+  return element.querySelectorAll('a').any((a) => a.attributes['name']?.startsWith('M') ?? false);
 }
