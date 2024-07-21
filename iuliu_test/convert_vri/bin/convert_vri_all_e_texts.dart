c567d6bee9cb73d7554868a8fcac78bb0428af38
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2024-07-21 18:20:57 -0400

    speed up all files convert

--- iuliu_test/convert_vri/bin/convert_vri_all_e_texts.dart
+++ iuliu_test/convert_vri/bin/convert_vri_all_e_texts.dart
@@ -1,8 +1,17 @@
 import 'package:convert_vri/create_sql_import_statements.dart';
 import 'package:convert_vri/split_pages.dart';
 import 'dart:io';
+import 'dart:isolate';
 
-void main() {
+void processInIsolate(Map<String, dynamic> data) {
+  final sendPort = data['sendPort'] as SendPort;
+  final bookHtml = data['bookHtml'] as String;
+  final name = data['name'] as String;
+  final result = generateFullBookImport(bookHtml, name);
+  sendPort.send(result);
+}
+
+void main() async {
   var currentDirectoryPath = Directory.current.resolveSymbolicLinksSync();
   final directory = Directory('$currentDirectoryPath/../e_texts/html').absolute;
   final List<FileSystemEntity> files = directory.listSync();
@@ -13,42 +22,53 @@ void main() {
     outputDirectory.createSync(recursive: true);
   }
 
-  for (int i = 0; i < files.length; i++) {
-    final file = files[i];
-    final count = 20 + i;
-    final bookId = 'annya_sadda_$count';
-    final bookHtml = File(file.path).readAsStringSync();
-    final pagesWithContent = extractMyanmarEditionPagesFromVriHtml(bookHtml);
-    final pagesWithContentWithParagraphs =
-        addParagraphsToPages(pagesWithContent);
-    final pagesWithContentWithParagraphsWithToc =
-        addTocsToPagesWithParagraphs(pagesWithContentWithParagraphs);
-    final bookInfo = extractBookInfo(pagesWithContentWithParagraphsWithToc);
+  final stopwatch = Stopwatch()..start();
+  print('Starting processing of ${files.length} files...');
 
-    final booksTableImport = createBookSQLImportStatement(bookId, bookInfo);
-    final tocsTableImport = createTocSQLImportStatements(
-            bookId, pagesWithContentWithParagraphsWithToc)
-        .join('\n');
-    final paragraphsTableImport = createParagraphsSQLImportStatements(
-            bookId, pagesWithContentWithParagraphsWithToc)
-        .join('\n');
-    final pagesTableImport = createPageSQLImportStatements(
-            bookId, pagesWithContentWithParagraphsWithToc)
-        .join('\n');
+  await Future.wait(files.asMap().entries.map((entry) async {
+    final index = entry.key;
+    final file = entry.value;
+    if (file is File) {
+      final bookHtml = await File(file.path).readAsString();
+      final receivePort = ReceivePort();
+      await Isolate.spawn(
+        processInIsolate,
+        {
+          'sendPort': receivePort.sendPort,
+          'bookHtml': bookHtml,
+          'name': 'annya_sadda_${index + 20}'
+        }
+      );
+      final fullBookImport = await receivePort.first;
+      final outputFilePath =
+          '${outputDirectory.path}/${file.uri.pathSegments.last.replaceAll('.html', '.sql')}';
+      await File(outputFilePath).writeAsString(fullBookImport);
+    }
+  }));
 
-    final fullBookImport = [
-      "DELETE FROM books where id='$bookId';",
-      booksTableImport,
-      "DELETE FROM tocs where book_id='$bookId';",
-      tocsTableImport,
-      "DELETE FROM paragraphs where book_id='$bookId';",
-      paragraphsTableImport,
-      "DELETE FROM pages where bookid='$bookId';",
-      pagesTableImport
-    ].join('\n');
+  stopwatch.stop();
+  print('Total processing time: ${stopwatch.elapsed}');
+}
 
-    final outputFilePath =
-        '${outputDirectory.path}/${file.uri.pathSegments.last.replaceAll('.html', '.sql')}';
-    File(outputFilePath).writeAsStringSync(fullBookImport);
-  }
+String generateFullBookImport(String bookHtml, String bookId) {
+  final pagesWithContent = extractMyanmarEditionPagesFromVriHtml(bookHtml);
+  final pagesWithContentWithParagraphs = addParagraphsToPages(pagesWithContent);
+  final pagesWithContentWithParagraphsWithToc =
+      addTocsToPagesWithParagraphs(pagesWithContentWithParagraphs);
+  final bookInfo = extractBookInfo(pagesWithContentWithParagraphsWithToc);
+
+  return [
+    "DELETE FROM books where id='$bookId';",
+    createBookSQLImportStatement(bookId, bookInfo),
+    "DELETE FROM tocs where book_id='$bookId';",
+    createTocSQLImportStatements(bookId, pagesWithContentWithParagraphsWithToc)
+        .join('\n'),
+    "DELETE FROM paragraphs where book_id='$bookId';",
+    createParagraphsSQLImportStatements(
+            bookId, pagesWithContentWithParagraphsWithToc)
+        .join('\n'),
+    "DELETE FROM pages where bookid='$bookId';",
+    createPageSQLImportStatements(bookId, pagesWithContentWithParagraphsWithToc)
+        .join('\n')
+  ].join('\n');
 }
