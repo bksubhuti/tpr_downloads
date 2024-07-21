ceb5821e4af32101c503f8d73ffe2ca324b39e16
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2024-07-21 19:18:40 -0400

    create extensions

--- iuliu_test/convert_vri/bin/convert_vri_all_e_texts.dart
+++ iuliu_test/convert_vri/bin/convert_vri_all_e_texts.dart
@@ -2,6 +2,7 @@ import 'package:convert_vri/create_sql_import_statements.dart';
 import 'package:convert_vri/split_pages.dart';
 import 'dart:io';
 import 'dart:isolate';
+import 'package:archive/archive.dart';
 
 void main() async {
   var currentDirectoryPath = Directory.current.resolveSymbolicLinksSync();
@@ -9,20 +10,61 @@ void main() async {
   final List<FileSystemEntity> files = directory.listSync();
   final outputDirectory =
       Directory('$currentDirectoryPath/../e_texts/sql').absolute;
+  final extensionsDirectory =
+      Directory('$currentDirectoryPath/../e_texts/extensions').absolute;
+
+  if (!outputDirectory.existsSync()) {
+    outputDirectory.createSync(recursive: true);
+  }
+
+  if (!extensionsDirectory.existsSync()) {
+    extensionsDirectory.createSync(recursive: true);
+  }
 
   print('Starting processing of ${files.length} files...');
   final stopwatch = Stopwatch()..start();
   await processFiles(files, outputDirectory);
   stopwatch.stop();
   print('Total processing time: ${stopwatch.elapsed}');
+
+  await Future.wait([
+    Category(
+      id: "annya_ledi_sayadaw",
+      name: "Leḍī sayāḍo gantha-saṅgaho",
+      books: [
+        "e0201n.nrf.sql",
+        "e0301n.nrf.sql",
+        "e0401n.nrf.sql",
+        "e0501n.nrf.sql"
+      ],
+    ),
+    Category(
+      id: "annya_buddha_vandana",
+      name: "Buddha-vandanā gantha-saṅgaho",
+      books: [
+        "e0901n.nrf.sql",
+        "e0902n.nrf.sql",
+        "e0903n.nrf.sql",
+        "e0904n.nrf.sql",
+        "e0905n.nrf.sql",
+        "e0906n.nrf.sql",
+        "e0907n.nrf.sql"
+      ],
+    ),
+  ].map((category) async {
+    List<String> fileContents = await Future.wait(category.books.map((file) => File('${outputDirectory.path}/$file').readAsString()));
+    final sqlFile = File("${extensionsDirectory.path}/${category.id}.sql");
+    await sqlFile.writeAsString([
+      createCategorySQLImportStatement(category.id, category.name),
+      ...fileContents
+    ].join('\n'));
+    final zipFile = File("${extensionsDirectory.path}/${category.id}.zip");
+    await createZipFromFile(sqlFile, zipFile);
+  }));
 }
 
 Future<void> processFiles(
     List<FileSystemEntity> files, Directory outputDirectory) async {
-  if (!outputDirectory.existsSync()) {
-    outputDirectory.createSync(recursive: true);
-  }
-
   await Future.wait(files.asMap().entries.map((entry) async {
     final index = entry.key;
     final file = entry.value as File;
@@ -80,4 +122,14 @@ class Category {
     required this.name,
     required this.books,
   });
+}
+
+Future<void> createZipFromFile(File sourceFile, File zipFile) async {
+  final archive = Archive();
+  final bytes = await sourceFile.readAsBytes();
+  final archiveFile =
+      ArchiveFile(sourceFile.path.split('/').last, bytes.length, bytes);
+  archive.addFile(archiveFile);
+  final zipData = ZipEncoder().encode(archive);
+  await zipFile.writeAsBytes(zipData!);
 }
